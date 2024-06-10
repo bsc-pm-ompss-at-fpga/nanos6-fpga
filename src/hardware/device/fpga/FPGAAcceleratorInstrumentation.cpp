059db23f786bef107d497229dfd809bd13a22a4b
Juan Miguel de Haro Ruiz <juan.deharoruiz@bsc.es>
2024-06-10 12:00:06 +0200

    Read all events after stopService is triggered

--- src/hardware/device/fpga/FPGAAcceleratorInstrumentation.cpp
+++ src/hardware/device/fpga/FPGAAcceleratorInstrumentation.cpp
@@ -1,6 +1,7 @@
 #include "FPGAAcceleratorInstrumentation.hpp"
 #include "InstrumentFPGAEvents.hpp"
 #include "lowlevel/FatalErrorHandler.hpp"
+#include <stack>
 
 void FPGAAcceleratorInstrumentation::serviceFunction(void *data)
 {
@@ -44,16 +45,19 @@ void FPGAAcceleratorInstrumentation::shutdownService() {
 }
 
 void FPGAAcceleratorInstrumentation::serviceLoop() {
+    constexpr int nEvents = 128;
     Instrument::startFPGAInstrumentation();
-    bool stopService_delayed = false;
-    while (!_stopService || stopService_delayed) {
-        xtasks_ins_event events[128];
-        xtasks_stat res = xtasksGetInstrumentData(handle.handle, events, 128);
+    bool getLastEvents = false;
+    while (!_stopService || getLastEvents) {
+        bool eventsFound = false;
+        xtasks_ins_event events[nEvents];
+        xtasks_stat res = xtasksGetInstrumentData(handle.handle, events, nEvents);
         FatalErrorHandler::failIf(
             res != XTASKS_SUCCESS,
             "Error while retrieving instrumentation events from handle with id: ", handle.info.id, ". xtasksGetInstrumentData returns: ", res
         );
-        for (int i = 0; i < 128 && events[i].eventType != XTASKS_EVENT_TYPE_INVALID; ++i) {
+        for (int i = 0; i < nEvents && events[i].eventType != XTASKS_EVENT_TYPE_INVALID; ++i) {
+            eventsFound = true;
             switch(events[i].eventType) {
             case XTASKS_EVENT_TYPE_BURST_OPEN:
             case XTASKS_EVENT_TYPE_BURST_CLOSE:
@@ -62,14 +66,14 @@ void FPGAAcceleratorInstrumentation::serviceLoop() {
             case XTASKS_EVENT_TYPE_POINT:
                 // Events has been lost
                 if (events[i].eventId == 82)
-                    std::cout << "Warning: Event Lost : " << events[i].eventType << "Type: " << events[i].value << std::endl;
+                    std::cout << "Warning: Event Lost : " << events[i].eventType << " Type: " << events[i].value << std::endl;
                 break;
             default:
-                std::cout << "Ignoring unkown fpga event type: " << events[i].eventType << "Type: " << events[i].value << std::endl;
+                std::cout << "Ignoring unkown fpga event type: " << events[i].eventType << " Type: " << events[i].value << std::endl;
             }
         }
-        if (stopService_delayed) stopService_delayed = false;
-        else if (!_stopService) stopService_delayed = true;
+        if (_stopService && !getLastEvents) getLastEvents = true;
+        else if (!eventsFound) getLastEvents = false;
     }
     Instrument::stopFPGAInstrumentation();
     _finishedService = true;
